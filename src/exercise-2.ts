a81775c0e4b6baefb5d79e5bb928b8a491526479
skrowll <lucasdejesus.rocha@hotmail.com>
2023-10-11 18:31:12 -0300

    feat: solve item d of exercise 2 (functional and imperative)

--- src/exercise-2.ts
+++ src/exercise-2.ts
@@ -65,22 +65,22 @@ function getNameByIdImperative(id: number): string {
 
 // Functional Paradigm
 
-function deleteByIdFunctional(id: number): number {
+function deleteByIdFunctional(id: number): string {
   const person = lista.find((person) => person.id === id);
   if (person) {
     const newLista = lista.filter((person) => person.id !== id)
     lista = newLista;
     // writeData(newLista)
-    return 1;
+    return 'Dado deletado com sucesso';
   };
-  return 0;
+  return 'Não foi possivel deletar o dado com o id informado';
 };
 
 // console.log(deleteByIdFunctional(5));
 
 // Imperative Paradigm
 
-function deleteByIdImperative(id: number): number {
+function deleteByIdImperative(id: number): string {
   let person = [];
   let newLista = [];
   for (let index = 0; index < lista.length; index += 1) {
@@ -95,9 +95,64 @@ function deleteByIdImperative(id: number): number {
       }
     }
     lista = newLista;
-    return 1;
+    // writeData(newLista)
+    return 'Dado deletado com sucesso';
   }
-  return 0;
+  return 'Não foi possivel deletar o dado com o id informado';
 }
 
 // console.log(deleteByIdImperative(1));
+
+// Exercício 2 - Item d) Função que altere a bio ou o name a partir de um id passado
+
+// Functional Paradigm
+
+function updateByIdFunctional(id: number, name: string, bio: string): string {
+  if (!id || id < 1 || !name || name === '' || !bio || bio === '') {
+    return 'Todos os parâmetros devem ser preenchidos corretamente';
+  };
+  const person = lista.find((person) => person.id === id);
+  if (person) {
+    const newLista = lista.map((person) => {
+      if (person.id === id) {
+        return { id, name, bio }
+      }
+      return person;
+    })
+    lista = newLista;
+    // writeData(newLista)    
+    return 'Dado atualizado com sucesso';
+  };
+  return 'Não foi possivel atualizar o dado com o id informado';
+};
+
+// console.log(updateByIdFunctional(3, 'Lucas Rocha', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida, nunc sed mollis cursus, ex nisl placerat nunc, vitae egestas sapien ante nec ipsum.'));
+
+// Imperative Paradigm
+
+function updateByIdImperative(id: number, name: string, bio: string): string {
+  if (!id || id < 1 || !name || name === '' || !bio || bio === '') {
+    return 'Todos os parâmetros devem ser preenchidos corretamente';
+  };
+  let person = [];
+  let newLista = [];
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index].id === id) {
+      person.push(lista[index]);
+    }
+  }
+  if (person.length > 0) {
+    for (let index = 0; index < lista.length; index += 1) {
+      if (lista[index].id !== id) {
+        newLista.push(lista[index]);
+      } else {
+        newLista.push({ id, name, bio });
+      }
+    }
+    lista = newLista;
+    return 'Dado atualizado com sucesso';
+  }
+  return 'Não foi possivel atualizar o dado com o id informado';
+}
+
+// console.log(updateByIdImperative(3, 'Lucas Rocha', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida, nunc sed mollis cursus, ex nisl placerat nunc, vitae egestas sapien ante nec ipsum.'));

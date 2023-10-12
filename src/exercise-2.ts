2338c2b4b6e4cfe4297d7c13e8bb40598620b7d0
skrowll <lucasdejesus.rocha@hotmail.com>
2023-10-12 10:37:27 -0300

    Merge branch 'develop' of github.com:skrowll/orion-bootcamp into feature/exercicio-2

---
+++ src/exercise-2.ts
@@ -0,0 +1,188 @@
+import * as fs from "fs";
+import IPerson from "./interfaces/IPerson.inteface";
+
+/**
+ * Lê os dados do arquivo JSON e os converte em uma lista de objetos do tipo IPerson.
+ *
+ * @returns Uma lista de objetos do tipo IPerson.
+ */
+function readData(): IPerson[] {
+  const data = fs.readFileSync("src/database/database.json");
+  return JSON.parse(data.toString());
+}
+
+/**
+ * Escreve os dados fornecidos no arquivo JSON.
+ *
+ * @param data - Uma lista de objetos do tipo IPerson a ser escrita no arquivo JSON.
+ */
+function writeData(data: IPerson[]): void {
+  const jsonData = JSON.stringify(data);
+  fs.writeFileSync("src/database/database.json", jsonData);
+}
+
+let lista: IPerson[] = readData();
+
+// Exercício 2 - Item a) Função que retorne a bio do id passado.
+
+/**
+ * Retorna a bio do objeto com o ID correspondente.
+ *
+ * @param id - O ID do objeto a ser procurado.
+ * @returns A bio do objeto ou uma mensagem de erro se o ID não for encontrado.
+ */
+function getBioByIdFunctional(id: number): string {
+  const person = lista.find((person) => person.id === id);
+  return person ? person.bio : "Id não encontrado";
+}
+
+/**
+ * Retorna a bio do objeto com o ID correspondente.
+ *
+ * @param id - O ID do objeto a ser procurado.
+ * @returns A bio do objeto ou uma mensagem de erro se o ID não for encontrado.
+ */
+function getBioByIdImperative(id: number): string {
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index].id === id) {
+      return lista[index].bio;
+    }
+  }
+  return "Id não encontrado";
+}
+
+// Exercício 2 - Item b) Função que retorne o name do id passado.
+
+/**
+ * Retorna o nome do objeto com o ID correspondente.
+ *
+ * @param id - O ID do objeto a ser procurado.
+ * @returns O nome do objeto ou uma mensagem de erro se o ID não for encontrado.
+ */
+function getNameByIdFunctional(id: number): string {
+  const person = lista.find((person) => person.id === id);
+  return person ? person.name : "Id não encontrado";
+}
+
+/**
+ * Retorna o nome do objeto com o ID correspondente.
+ *
+ * @param id - O ID do objeto a ser procurado.
+ * @returns O nome do objeto ou uma mensagem de erro se o ID não for encontrado.
+ */
+function getNameByIdImperative(id: number): string {
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index].id === id) {
+      return lista[index].name;
+    }
+  }
+  return "Id não encontrado";
+}
+
+// Exercício 2 - Item c) Função que apague um item da lista a partir de um id passado
+
+/**
+ * Deleta o objeto com o ID correspondente da lista.
+ *
+ * @param id - O ID do objeto a ser deletado.
+ * @returns Uma mensagem de sucesso ou erro.
+ */
+function deleteByIdFunctional(id: number): string {
+  const person = lista.find((person) => person.id === id);
+  if (person) {
+    const newLista = lista.filter((person) => person.id !== id);
+    lista = newLista;
+    // writeData(newLista)
+    return "Dado deletado com sucesso";
+  }
+  return "Não foi possivel deletar o dado com o id informado";
+}
+
+/**
+ * Deleta o objeto com o ID correspondente da lista.
+ *
+ * @param id - O ID do objeto a ser deletado.
+ * @returns Uma mensagem de sucesso ou erro.
+ */
+function deleteByIdImperative(id: number): string {
+  let person = [];
+  let newLista = [];
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index].id === id) {
+      person.push(lista[index]);
+    }
+  }
+  if (person.length > 0) {
+    for (let index = 0; index < lista.length; index += 1) {
+      if (lista[index].id !== id) {
+        newLista.push(lista[index]);
+      }
+    }
+    lista = newLista;
+    // writeData(newLista)
+    return "Dado deletado com sucesso";
+  }
+  return "Não foi possivel deletar o dado com o id informado";
+}
+
+// Exercício 2 - Item d) Função que altere a bio ou o name a partir de um id passado
+
+/**
+ * Atualiza o objeto com o ID correspondente com novos valores.
+ *
+ * @param id - O ID do objeto a ser atualizado.
+ * @param name - O novo nome a ser atribuído ao objeto.
+ * @param bio - A nova bio a ser atribuída ao objeto.
+ * @returns Uma mensagem de sucesso ou erro.
+ */
+function updateByIdFunctional(id: number, name: string, bio: string): string {
+  if (!id || id < 1 || !name || name === "" || !bio || bio === "") {
+    return "Todos os parâmetros devem ser preenchidos corretamente";
+  }
+  const person = lista.find((person) => person.id === id);
+  if (person) {
+    const newLista = lista.map((person) => {
+      if (person.id === id) {
+        return { id, name, bio };
+      }
+      return person;
+    });
+    lista = newLista;
+    // writeData(newLista)
+    return "Dado atualizado com sucesso";
+  }
+  return "Não foi possivel atualizar o dado com o id informado";
+}
+
+/**
+ * Atualiza o objeto com o ID correspondente com novos valores.
+ *
+ * @param id - O ID do objeto a ser atualizado.
+ * @param name - O novo nome a ser atribuído ao objeto.
+ * @param bio - A nova bio a ser atribuída ao objeto.
+ * @returns Uma mensagem de sucesso ou erro.
+ */
+function updateByIdImperative(id: number, name: string, bio: string): string {
+  if (!id || id < 1 || !name || name === "" || !bio || bio === "") {
+    return "Todos os parâmetros devem ser preenchidos corretamente";
+  }
+  let person = [];
+  let newLista = [];
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index].id === id) {
+      person.push(lista[index]);
+    }
+  }
+  if (person.length > 0) {
+    for (let index = 0; index < lista.length; index += 1) {
+      if (lista[index].id !== id) {
+        newLista.push(lista[index]);
+      } else {
+        newLista.push({ id, name, bio });
+      }
+    }
+    lista = newLista;
+    return "Dado atualizado com sucesso";
+  }
+  return "Não foi possivel atualizar o dado com o id informado";
+}
---
+++ src/interfaces/IPerson.inteface.ts
@@ -0,0 +1,7 @@
+interface IPerson {
+  id: number;
+  name: string;
+  bio: string;
+};
+
+export default IPerson;
